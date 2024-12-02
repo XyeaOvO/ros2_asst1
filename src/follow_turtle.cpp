4d87b7fa573bf2a4fdb4161aeaa1f624be165f57
mo <mo@bupt.edu.cn>
2024-12-02 11:07:29 +0800

    follow

--- src/follow_turtle.cpp
+++ src/follow_turtle.cpp
@@ -15,34 +15,45 @@ private:
     std::string target,catcher;
     bool reach_flag;
 
-    // Well, these two variables are not used in the code, but they may help in the task
-    bool inital_flag;
-    geometry_msgs::msg::TransformStamped tf_inital;
+    // PID控制器变量
+    double Kp_linear, Ki_linear, Kd_linear;
+    double Kp_angular, Ki_angular, Kd_angular;
+
+    double prev_error_linear, prev_error_angular;
+    double integral_linear, integral_angular;
 
     void timer_callback()
     {
-
         geometry_msgs::msg::TransformStamped tf;     
         try
         {
             tf = tf_buffer_->lookupTransform(catcher, target, tf2::TimePointZero);
-
-        } catch (tf2::TransformException &ex)
+        } 
+        catch (tf2::TransformException &ex)
         {
             RCLCPP_ERROR(this->get_logger(), "Could not transform %s to %s: %s", catcher.c_str(), target.c_str(), ex.what());
             return;
         }
 
-        // now the code was implemented to make the catcher follow the target
-        // you should modify it to keep the relative position and orientation between the target and the catcher
-        // you can first try to make the catcher keep a fixed distance from the target
-        // then you can try to make the catcher keep a fixed angle from the target
-        // these two tasks are not easy, but you can do it!
-
         auto t = tf.transform;
         auto message = geometry_msgs::msg::Twist();
 
-        if (hypot(t.translation.x, t.translation.y) < 0.1)
+        // 计算线性误差和角度误差
+        double error_linear = hypot(t.translation.x, t.translation.y); 
+        double error_angular = atan2(t.translation.y, t.translation.x);
+
+        // PID控制计算
+        double control_linear = Kp_linear * error_linear + Ki_linear * integral_linear + Kd_linear * (error_linear - prev_error_linear);
+        double control_angular = Kp_angular * error_angular + Ki_angular * integral_angular + Kd_angular * (error_angular - prev_error_angular);
+
+        // 更新积分项和前一个误差
+        integral_linear += error_linear;
+        integral_angular += error_angular;
+        prev_error_linear = error_linear;
+        prev_error_angular = error_angular;
+
+        // 如果误差小于阈值，停止
+        if (error_linear < 0.1)
         {
             message.linear.x = 0.0;
             message.angular.z = 0.0;
@@ -56,12 +67,33 @@ private:
             reach_flag = false;
         }
 
-        message.linear.x = 0.5 * hypot(t.translation.x, t.translation.y);
-        message.angular.z = 1.0 * atan2(t.translation.y, t.translation.x);
+        // 设定限额
+        const double linear_min = -100.0;
+        const double linear_max = 100.0;
+        const double angular_min = -1000.0;
+        const double angular_max = 1000.0;
+
+        // 限制线速度
+        if (control_linear < linear_min) {
+            control_linear = linear_min;
+        } else if (control_linear > linear_max) {
+            control_linear = linear_max;
+        }
+
+        // 限制角速度
+        if (control_angular < angular_min) {
+            control_angular = angular_min;
+        } else if (control_angular > angular_max) {
+            control_angular = angular_max;
+        }
+
+        // 发布控制命令
+        message.linear.x = control_linear;
+        message.angular.z = control_angular;
         RCLCPP_INFO(this->get_logger(), "Publishing: linear.x: '%f', angular.z: '%f'", message.linear.x, message.angular.z);
         publisher_->publish(message);
-
     }
+
 public: 
     node(std::string target, std::string catcher): Node("follower"), target(target), catcher(catcher)
     {
@@ -71,8 +103,19 @@ public:
             std::chrono::milliseconds(100),
             std::bind(&node::timer_callback, this));
         publisher_ = this->create_publisher<geometry_msgs::msg::Twist>(catcher+"/cmd_vel", 10);
+
+        // 初始化PID控制参数
+        Kp_linear = 3; Ki_linear = 0.005; Kd_linear = 0.2;
+        Kp_angular = 15; Ki_angular = 0.0; Kd_angular = 0;
+
+        prev_error_linear = 0.0;
+        prev_error_angular = 0.0;
+        integral_linear = 0.0;
+        integral_angular = 0.0;
+
         RCLCPP_INFO(this->get_logger(), "Hello, world");   
     }
+
     ~node()
     {
         RCLCPP_INFO(this->get_logger(), "Goodbye, world");
