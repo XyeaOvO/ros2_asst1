df6232b6b2f1cf243242621211fc52ee8ef23838
mo <mo@bupt.edu.cn>
2024-12-02 19:36:58 +0800

    支持发布所有海龟的tf2变换信息，实现2只海龟跟随1只海龟

--- src/follow_turtle.cpp
+++ src/follow_turtle.cpp
@@ -4,21 +4,22 @@
 #include "tf2_ros/transform_listener.h"
 #include "tf2_ros/buffer.h"
 #include "tf2/exceptions.h"
+#include <vector>
+#include <memory>
 
-class node: public rclcpp::Node
+class FollowerNode: public rclcpp::Node
 {
 private:
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
     std::shared_ptr<tf2_ros::TransformListener> tf_listener_{nullptr};
     std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
-    std::string target,catcher;
+    std::string target, catcher;
     bool reach_flag;
 
     // PID控制器变量
     double Kp_linear, Ki_linear, Kd_linear;
     double Kp_angular, Ki_angular, Kd_angular;
-
     double prev_error_linear, prev_error_angular;
     double integral_linear, integral_angular;
 
@@ -31,7 +32,8 @@ private:
         } 
         catch (tf2::TransformException &ex)
         {
-            RCLCPP_ERROR(this->get_logger(), "Could not transform %s to %s: %s", catcher.c_str(), target.c_str(), ex.what());
+            RCLCPP_ERROR(this->get_logger(), "Could not transform %s to %s: %s", 
+                        catcher.c_str(), target.c_str(), ex.what());
             return;
         }
 
@@ -43,8 +45,12 @@ private:
         double error_angular = atan2(t.translation.y, t.translation.x);
 
         // PID控制计算
-        double control_linear = Kp_linear * error_linear + Ki_linear * integral_linear + Kd_linear * (error_linear - prev_error_linear);
-        double control_angular = Kp_angular * error_angular + Ki_angular * integral_angular + Kd_angular * (error_angular - prev_error_angular);
+        double control_linear = Kp_linear * error_linear + 
+                              Ki_linear * integral_linear + 
+                              Kd_linear * (error_linear - prev_error_linear);
+        double control_angular = Kp_angular * error_angular + 
+                               Ki_angular * integral_angular + 
+                               Kd_angular * (error_angular - prev_error_angular);
 
         // 更新积分项和前一个误差
         integral_linear += error_linear;
@@ -58,7 +64,10 @@ private:
             message.linear.x = 0.0;
             message.angular.z = 0.0;
             publisher_->publish(message);
-            if (!reach_flag) RCLCPP_INFO(this->get_logger(), "Reached target");
+            if (!reach_flag) {
+                RCLCPP_INFO(this->get_logger(), "%s reached target %s", 
+                           catcher.c_str(), target.c_str());
+            }
             reach_flag = true;
             return;
         }
@@ -67,82 +76,84 @@ private:
             reach_flag = false;
         }
 
-        // 设定限额
-        const double linear_min = -100.0;
-        const double linear_max = 100.0;
-        const double angular_min = -1000.0;
-        const double angular_max = 1000.0;
-
-        // 限制线速度
-        if (control_linear < linear_min) {
-            control_linear = linear_min;
-        } else if (control_linear > linear_max) {
-            control_linear = linear_max;
-        }
-
-        // 限制角速度
-        if (control_angular < angular_min) {
-            control_angular = angular_min;
-        } else if (control_angular > angular_max) {
-            control_angular = angular_max;
-        }
+        // 设定限额并限制速度
+        control_linear = std::clamp(control_linear, -100.0, 100.0);
+        control_angular = std::clamp(control_angular, -100.0, 100.0);
 
         // 发布控制命令
         message.linear.x = control_linear;
         message.angular.z = control_angular;
-        RCLCPP_INFO(this->get_logger(), "Publishing: linear.x: '%f', angular.z: '%f'", message.linear.x, message.angular.z);
         publisher_->publish(message);
     }
 
 public: 
-    node(std::string target, std::string catcher): Node("follower"), target(target), catcher(catcher)
+    FollowerNode(const std::string& target, const std::string& catcher)
+        : Node("follower_" + catcher), target(target), catcher(catcher)
     {
         tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
         tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+        
         timer_ = this->create_wall_timer(
             std::chrono::milliseconds(100),
-            std::bind(&node::timer_callback, this));
-        publisher_ = this->create_publisher<geometry_msgs::msg::Twist>(catcher+"/cmd_vel", 10);
+            std::bind(&FollowerNode::timer_callback, this));
+            
+        publisher_ = this->create_publisher<geometry_msgs::msg::Twist>(
+            catcher + "/cmd_vel", 10);
 
         // 初始化PID控制参数
-        Kp_linear = 3; Ki_linear = 0.005; Kd_linear = 0.2;
-        Kp_angular = 15; Ki_angular = 0.0; Kd_angular = 0;
+        Kp_linear = 3.0; Ki_linear = 0.005; Kd_linear = 0.2;
+        Kp_angular = 15.0; Ki_angular = 0.0; Kd_angular = 0.0;
 
         prev_error_linear = 0.0;
         prev_error_angular = 0.0;
         integral_linear = 0.0;
         integral_angular = 0.0;
 
-        RCLCPP_INFO(this->get_logger(), "Hello, world");   
+        RCLCPP_INFO(this->get_logger(), "Started follower for %s following %s", 
+                    catcher.c_str(), target.c_str());   
     }
 
-    ~node()
+    ~FollowerNode()
     {
-        RCLCPP_INFO(this->get_logger(), "Goodbye, world");
+        RCLCPP_INFO(this->get_logger(), "Shutting down follower for %s", catcher.c_str());
     }
 };
 
-class ROS_EVENT_LOOP
+class MultiFollowerManager 
 {
+private:
+    std::vector<std::shared_ptr<FollowerNode>> nodes;
+    rclcpp::executors::MultiThreadedExecutor executor;
+
 public:
-    ROS_EVENT_LOOP(int argc, char *argv[], std::string target,std::string catcher)
-    {
-        rclcpp::init(argc, argv);
-        rclcpp::spin(std::make_shared<node>(target,catcher));
+    void addFollower(const std::string& target, const std::string& follower) {
+        nodes.push_back(std::make_shared<FollowerNode>(target, follower));
+        executor.add_node(nodes.back());
     }
-    ~ROS_EVENT_LOOP()
-    {
-        rclcpp::shutdown();
+
+    void spin() {
+        executor.spin();
     }
 };
 
 int main(int argc, char *argv[])
 {
-    if (argc != 3 )
-    {
-        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "usage: follow target catcher");
+    if (argc != 4) {
+        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), 
+                    "Usage: follow target follower1 follower2");
         return 1;
     }
-    ROS_EVENT_LOOP(argc, argv, argv[1], argv[2]);
+
+    rclcpp::init(argc, argv);
+    
+    MultiFollowerManager manager;
+    
+    // 添加两个跟随者，都跟随同一个目标
+    manager.addFollower(argv[1], argv[2]);  // follower1 follows target
+    manager.addFollower(argv[1], argv[3]);  // follower2 follows target
+    
+    manager.spin();
+    
+    rclcpp::shutdown();
     return 0;
 }
--- src/tf2_node.cpp
+++ src/tf2_node.cpp
@@ -3,14 +3,16 @@
 #include "tf2/LinearMath/Quaternion.h"
 #include "geometry_msgs/msg/pose.hpp"
 #include "turtlesim/msg/pose.hpp"
+#include <vector>
+#include <memory>
 
-class node: public rclcpp::Node
+class TFNode: public rclcpp::Node
 {
 private:
     tf2_ros::TransformBroadcaster tfb;
     std::string turtleName;
     rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr subscription_;
-
+    
     void pose_callback(const turtlesim::msg::Pose::SharedPtr msg)
     {
         geometry_msgs::msg::TransformStamped t;
@@ -28,40 +30,60 @@ private:
         t.transform.rotation.w = q.w();
         tfb.sendTransform(t);
     }
+
 public:
-    node(std::string st): Node(st + "_boardcaster"), tfb(this), turtleName(st)
+    TFNode(const std::string& st)
+        : Node(st + "_broadcaster"), tfb(this), turtleName(st)
     {
         subscription_ = this->create_subscription<turtlesim::msg::Pose>(
-            turtleName+"/pose", 10, std::bind(&node::pose_callback, this, std::placeholders::_1));
-        RCLCPP_INFO(this->get_logger(), "Hello, world");
+            turtleName + "/pose", 10,
+            std::bind(&TFNode::pose_callback, this, std::placeholders::_1));
+        RCLCPP_INFO(this->get_logger(), "Started TF broadcaster for %s", turtleName.c_str());
     }
-    ~node()
+
+    ~TFNode()
     {
-        RCLCPP_INFO(this->get_logger(), "Goodbye, world");
+        RCLCPP_INFO(this->get_logger(), "Shutting down TF broadcaster for %s", turtleName.c_str());
     }
 };
 
-class ROS_EVENT_LOOP
+class MultiTFManager 
 {
+private:
+    std::vector<std::shared_ptr<TFNode>> nodes;
+    rclcpp::executors::MultiThreadedExecutor executor;
+
 public:
-    ROS_EVENT_LOOP(int argc, char *argv[],std::string st)
-    {
-        rclcpp::init(argc, argv);
-        rclcpp::spin(std::make_shared<node>(st));
+    void addTurtle(const std::string& turtleName) {
+        nodes.push_back(std::make_shared<TFNode>(turtleName));
+        executor.add_node(nodes.back());
     }
-    ~ROS_EVENT_LOOP()
-    {
-        rclcpp::shutdown();
+
+    void spin() {
+        executor.spin();
     }
 };
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2 )
-    {
-        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "usage: tf turtle_name");
+    if (argc < 2) {
+        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), 
+                     "Usage: tf turtle1_name [turtle2_name turtle3_name ...]");
         return 1;
     }
-    ROS_EVENT_LOOP(argc, argv, argv[1]);
+
+    rclcpp::init(argc, argv);
+    
+    MultiTFManager manager;
+    
+    // Add all turtles specified in command line arguments
+    for (int i = 1; i < argc; ++i) {
+        manager.addTurtle(argv[i]);
+    }
+    
+    // Spin all nodes
+    manager.spin();
+    
+    rclcpp::shutdown();
     return 0;
 }
